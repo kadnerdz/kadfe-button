e6a940f1fd5f374916bff2458b88150f5e800ca1
KingWalrus <chonigman@gmail.com>
2017-04-05 02:07:55 +0000

    Add kadfe client and set up socket2me bro

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 var rpio = require('rpio');
+var kadfe = require('kadfe-client');
 rpio.init({mapping: 'gpio'});
 rpio.open(14, rpio.INPUT, rpio.PULL_DOWN);
 var pressed = false;
+var available = false;
+
+kadfe.openSocket()
+    .then((socket) => {
+        socket.on('message', (message) => {
+            if (message === 'available'){
+                console.log('Available')
+                available = true;    
+            } 
+            if (message === 'unavailable'){
+                available = false;     
+                console.log('Unavailable')
+            }
+        })
+    }).catch((error) => {
+        console.log(`websocket attempt failed: ${error}`)
+    });
+
 
 function pollb(pin){
     var state = rpio.read(pin) ? 'pressed' : 'released';
-    if (state == 'pressed' && pressed == false){
-        console.log(Date());
-        console.log('Button Event ' + state);
-        console.log('');
+    if (state == 'pressed' && pressed == false && available == false){
         pressed = true;
-    } else if(state == 'released' && pressed == true){
-        console.log('Released');
+    } else if(state == 'released' && pressed == true && available == false){
+        pressed = false;
+        console.log('Coffee has been made')
+        kadfe.makeCoffee();
+    } else if (state == 'pressed' && pressed == false && available == true){
+        pressed = true;
+    } else if (state == 'released' && pressed == true && available == true){
+        kadfe.clearCoffee();
+        console.log('Coffee has been taken')
         pressed = false;
     }
 }

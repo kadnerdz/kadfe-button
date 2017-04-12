682ad7f356d89acf27e9da704c6cfaf8e304749f
KingWalrus <chonigman@gmail.com>
2017-04-12 02:04:48 +0000

    Update to work with shield pins and with led

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var rpio = require('rpio');
 var kadfe = require('kadfe-client');
+var button_pin = 4;
+var led_pin = 17;
 rpio.init({mapping: 'gpio'});
-rpio.open(14, rpio.INPUT, rpio.PULL_DOWN);
+rpio.open(button_pin, rpio.INPUT, rpio.PULL_DOWN);
+rpio.open(led_pin, rpio.OUTPUT, rpio.HIGH);
 var pressed = false;
 var available = false;
 
@@ -27,17 +30,21 @@ function pollb(pin){
     var state = rpio.read(pin) ? 'pressed' : 'released';
     if (state == 'pressed' && pressed == false && available == false){
         pressed = true;
+        console.log('Button is Pressed \n');
     } else if(state == 'released' && pressed == true && available == false){
         pressed = false;
-        console.log('Coffee has been made')
+        console.log('Button is Released');
+        console.log('Coffee has been made: Pressed - ' + pressed + ' Available - ' + available + '\n')
+        rpio.write(led_pin, rpio.HIGH);
         kadfe.makeCoffee();
     } else if (state == 'pressed' && pressed == false && available == true){
         pressed = true;
     } else if (state == 'released' && pressed == true && available == true){
         kadfe.clearCoffee();
-        console.log('Coffee has been taken')
+        console.log('Coffee has been taken: Pressed - ' + pressed + ' Available - ' + available + '\n')
+        rpio.write(led_pin, rpio.LOW);
         pressed = false;
     }
 }
 
-rpio.poll(14, pollb);
+rpio.poll(button_pin, pollb);
